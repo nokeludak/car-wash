a4ff782f2dd297b9f0f0aaa7033a60695924efe2
nokeludak <ksmajic996@gmail.com>
2022-04-01 20:13:49 +0200

    Commit register and login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Home from "./pages/Home"
+import Register from './pages/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Register />
     </div>
   );
 }
